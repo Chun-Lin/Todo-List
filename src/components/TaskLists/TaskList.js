98cc85ab231846e12783163ad564b9d3b4a9c989
Chun-Lin <wulin40063@gmail.com>
2018-06-08 18:39:54 +0800

    feat: add font-awesome packages

--- src/components/TaskLists/TaskList.js
+++ src/components/TaskLists/TaskList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+
 import { skin } from '../../assets/colors'
 
 const StyledTaskList = styled.li`
